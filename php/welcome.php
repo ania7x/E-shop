e5a3caa4ecb145682536070fa117e0c08dfeb777
ania7x <33523995+ania7x@users.noreply.github.com>
2022-11-22 19:29:47 +0200

    user shown on top right corner

---
+++ php/welcome.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+$_SESSION['id']="fewijfeo";
+$_SESSION['username']="ania";
+$_SESSION['surname']="gewfwe";
+$_SESSION['role']="fdf";
+if (empty($_SESSION['id'])):
+  header('Location:login_user.php');
+endif;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/welcomestyle.css">
+  <title>E-shopper</title>
+</head>
+
+<body>
+  <div class='head'>
+  <h1>E-shopper</h1>
+    <ul>
+      <li>
+        <a href="#">Menu</a>
+        <ul class="dropdown">
+          <li><a href="../pages/welcome.html">Welcome</a></li>
+          <li><a href="../index.html">Login</a></li>
+          <li><a href="../pages/signup.html">Sign up</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">Cart</a></li>
+          <li><a href="#">Seller</a></li>
+          <li><a href="#">Administration</a></li>
+        </ul>
+      </li>
+    </ul>
+    <a href="logout.php">
+      <div style="float:right"><button>Logout</button>
+      </div>
+    </a>
+  </div>
+  <div class='maincontent'>
+  <h4 class='company'>Welcome
+      <?php echo $_SESSION['username'] ." ".$_SESSION['surname']." (".$_SESSION['role']." )" ;?>
+  </h4>
+</div>
+</body>
+
+</html>
