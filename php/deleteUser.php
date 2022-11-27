704b4b57eddacf7faa5801df7dda906604b6eb70
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-11-27 17:03:16 +0200

    add delete user functionality

---
+++ php/deleteUser.php
@@ -0,0 +1,25 @@
+<?php
+    $mysqli = mysqli_connect("localhost","admin","admin","cloud");
+
+    if(mysqli_connect_errno()){
+        die(mysqli_connect_error());
+    }
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(isset($_POST['id'])){
+            $id = $_POST['id'];
+            $query = "DELETE FROM users WHERE ID = ".$id.";";
+            $res = mysqli_query($mysqli, $query);
+            if($res){
+                $data["message"] = "success";
+                echo json_encode($data);
+                exit();
+            }
+            echo mysqli_error($mysqli);
+            exit(1);
+        }
+        $error["error"] = "No id passed";
+        echo json_encode($error);
+        exit(1);
+    }
+    echo "Something went wrong";
+?>
