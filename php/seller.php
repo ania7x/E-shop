8cb9f79e73d659d19dba1c7e1b03bfde5159ce26
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-12-04 16:37:10 +0200

    choose products depending on the seller

---
+++ php/seller.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$mysqli = mysqli_connect("localhost", "admin", "admin", "cloud");
+
+if (mysqli_connect_errno()) {
+    die(mysqli_connect_error());
+}
+
+$query = "SELECT p.ID as pId, p.NAME AS pName, PRICE, CATEGORY, DATEOFWITHDRAWAL
+            FROM cloud.products p 
+            WHERE p.SELLERID = ".$_SESSION['id'].";";
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $res = mysqli_query($mysqli, $query);
+
+    while ($row = mysqli_fetch_array($res)) {
+        $products[] = $row;
+    }
+    echo json_encode($products);
+} else {
+    echo "{\"message\":\"Problem\"}";
+}
+?>
