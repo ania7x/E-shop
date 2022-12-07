5a8d7664815038d2163363ec0e9583a73eda379d
Panagiotis Savvaidis <panagiotissav@gmail.com>
2022-12-07 03:05:35 +0200

    Stylize navbar

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,20 +1,22 @@
-<div class='head'>
-  <h1 class='company'>E-shopper</h1>
-</div>
-<ul class="navbar">
-  <li class="masterItem">
-    <a href="#">Menu</a>
-    <ul class="dropdown">
-      <li><a href="./welcome.php">Welcome</a></li>
-      <li><a href="../index.html">Login</a></li>
-      <li><a href="./signup.html">Sign up</a></li>
-      <li><a href="./products.php">Products</a></li>
-      <li><a href="./carts.php">Cart</a></li>
-      <li><a href="./sellers.php">Seller</a></li>
-      <li><a href="./administration.php">Administration</a></li>
-    </ul>
-  </li>
-</ul>
-<a class="logout-btn" href="../php/logout.php">
-  Logout
-</a>
+<header class="header">
+  <div class='head'>
+    <h1 class='company'>E-shopper</h1>
+  </div>
+  <ul class="navbar">
+    <li class="masterItem">
+      <a href="#">Menu</a>
+      <ul class="dropdown">
+        <li><a href="./welcome.php">Welcome</a></li>
+        <li><a href="../index.html">Login</a></li>
+        <li><a href="./signup.html">Sign up</a></li>
+        <li><a href="./products.php">Products</a></li>
+        <li><a href="./carts.php">Cart</a></li>
+        <li><a href="./sellers.php">Seller</a></li>
+        <li><a href="./administration.php">Administration</a></li>
+      </ul>
+    </li>
+  </ul>
+  <a class="logout-btn" href="../php/logout.php">
+    Logout
+  </a>
+</header>
